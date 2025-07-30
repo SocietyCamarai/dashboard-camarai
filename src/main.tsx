1ab2a60b261b74bee7129ad73be9a8b83a3c2639
Keny Camarai <society@camarai.es>
2025-07-30 13:01:29 +0200

    Fix SpeedInsights Vercel

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   // <StrictMode>
-    <App />
+    <>
+      <App />
+      <SpeedInsights />
+    </>
   // </StrictMode>,
 )
